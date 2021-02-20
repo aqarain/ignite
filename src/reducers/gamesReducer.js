a8bf0ea06b451026c588beafb5e103d95bf39fcd
Atif Qayyum <aqarain92@gmail.com>
2021-02-21 02:44:04 +0500

    Fetching upcoming and new games

--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -1,14 +1,19 @@
 const initState = {
   popular: [],
-  newGames: [],
   upcoming: [],
+  newGames: [],
   searched: []
 };
 
 const gamesReducer = (state = initState, action) => {
   switch (action.type) {
     case "FETCH_GAMES":
-      return { ...state, popular: action.payload.popular };
+      return {
+        ...state,
+        popular: action.payload.popular,
+        upcoming: action.payload.upcoming,
+        newGames: action.payload.newGames
+      };
     default:
       return { ...state };
   }
