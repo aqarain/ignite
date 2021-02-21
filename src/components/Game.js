4c607dc8069d54dc474f6d4de3760f65fb9d6fcb
Atif Qayyum <aqarain92@gmail.com>
2021-02-21 17:38:52 +0500

    Toggle Components

--- src/components/Game.js
+++ src/components/Game.js
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom";
 const Game = ({ id, name, released, image }) => {
   const dispatch = useDispatch();
   const loadDetailHandler = () => {
+    document.body.style.overflow = "hidden";
     dispatch(loadDetail(id));
   };
 
@@ -27,6 +28,7 @@ const StyledGame = styled(motion.div)`
   text-align: center;
   border-radius: 1rem;
   cursor: pointer;
+  overflow: hidden;
   img {
     width: 100%;
     height: 40vh;
